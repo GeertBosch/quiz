0a976cb408f5b20776df62525a011a070be29401
Geert Bosch <bosch@gnat.com>
2017-09-23 11:57:27 -0400

    Add autosave

--- wordquiz.py
+++ wordquiz.py
@@ -1,22 +1,53 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
+import os
 import random
+import sys
+import time
 
-repetitions = 3
-quizSize = 10
-wordList = []
+repetitions = 1
+quizSize = 5
 
-encouragements = [ 
+if not "DEBUG" in os.environ:
+    sys.tracebacklimit = 0
+
+encouragements = [
     "✌️  Victory is yours!",
     "✋  High five!",
     "💪  Going strong!",
-    "👌  That's a-OK!",
+    "👌  That's A-OK!",
     "👊  Fist bump!",
     "👏  Applause!",
     "🤘  Rock on!",
-    "😎  Cool!" ]
+    "😎  Cool!"]
+
+
+def waitKey():
+    ''' Wait for a key press on the console and return it. '''
+    result = None
+    if os.name == 'nt':
+        import msvcrt
+        result = msvcrt.getch()
+    else:
+        import termios
+        fd = sys.stdin.fileno()
+        oldterm = termios.tcgetattr(fd)
+        newattr = termios.tcgetattr(fd)
+        newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+        termios.tcsetattr(fd, termios.TCSANOW, newattr)
+        try:
+            result = sys.stdin.read(1)
+            time.sleep(0.5)
+            print("result: " + str(result))
+            time.sleep(1)
+        except IOError:
+            print("Error!")
+            time.sleep(1)
+            pass
+        finally:
+            termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+    return result
 
 
 def CSI(item):
@@ -26,13 +57,19 @@ def CSI(item):
 def setCol(col):
     return CSI(str(col) + 'G')
 
+
 def setRowCol(row, col):
     return CSI(str(row) + ';' + str(col) + 'H')
 
 
 def readWordList(filename):
-    wordList.extend(map(lambda line: tuple(line.split()),
-                        open(filename, "r").readlines()))
+    print "reading file " + filename
+    return map(lambda line: tuple(line.split()), open(filename, "r").readlines())
+
+
+def writeWordList(filename, list):
+    open(filename, 'w').writelines(
+        map(lambda item: '\t'.join(item) + '\n', list))
 
 
 def makeQuiz(wordList, count):
@@ -52,14 +89,14 @@ def makeQuiz(wordList, count):
 
 
 def askOne(pair):
-    print('');
+    print('')
     answer = raw_input(setCol(17) + pair[0] + setCol(41) + '? ')
-    correct = answer == pair[1];
+    correct = answer == pair[1]
     if correct:
         print(CSI('F') + setCol(41) + '  ' + answer)
     else:
         print(CSI('F') + setCol(41) +
-              CSI('31m') + '✗ ' + CSI('m') + answer 
+              CSI('31m') + '✗ ' + CSI('m') + answer
                          + ' \t' + CSI('4m') + CSI('31m') + pair[1]) + CSI('m')
     return (pair, correct)
 
@@ -67,23 +104,25 @@ def askOne(pair):
 def takeQuiz(quiz):
     print(setRowCol(3, 20) + '     q⃣   u⃣   i⃣   z⃣     ')
     print('')
-    result = [];
+    result = []
     for item in quiz:
         result.append(askOne(item))
 
     return result
+
 
 def showStats(wordList, wordsToLearn):
     pctLearned = 100 - len(wordsToLearn) * 100 / (repetitions * len(wordList))
     print(setRowCol(2, 60) + '  Words: {:3d}'.format(len(wordList)))
     print(setRowCol(3, 60) + 'Learned: {:3.0f}%'.format(pctLearned))
 
+
 def learnWords(wordList, wordsToLearn):
     while len(wordsToLearn):
         quiz = makeQuiz(wordsToLearn, quizSize)
         print(CSI('2J'))
         showStats(wordList, wordsToLearn)
-        result=takeQuiz(quiz)
+        result = takeQuiz(quiz)
         perfect = all(item[1] for item in result)
 
         for item in result:
@@ -100,20 +139,40 @@ def learnWords(wordList, wordsToLearn):
             print(setRowCol(10 + quizSize * 2, 17) + '💯  Perfect!\n\n')
             break
 
-        msg = (random.sample(encouragements, 1)[0] + " Enter to continue." 
+        msg = (random.sample(encouragements, 1)[0] + " Enter to continue."
                if perfect else 'Enter to continue or Ctrl-C to stop.')
 
-        raw_input(setRowCol(10 + quizSize * 2, 17) + msg)
+        print(setRowCol(10 + quizSize * 2, 17) + msg)
+        waitKey()
+        # raw_input(setRowCol(10 + quizSize * 2, 17) + msg)
 
 
-for file in (sys.argv[1:] if len(sys.argv) > 1 else ["wordList.txt"]):
-    wordList = []
-    readWordList(file)
+def learnFile(filename):
+    savename = filename + '.save'
+    wordList = list(set(readWordList(filename)))
 
-wordList = list(set(wordList))
-wordsToLearn = repetitions * wordList
+    if os.path.isfile(savename):
+        print("Resuming progress for " + filename)
+        time.sleep(0.5)
+        wordsToLearn = readWordList(savename)
+        os.remove(savename)
+    else:
+        wordsToLearn = repetitions * wordList
 
-try:
-    learnWords(wordList, wordsToLearn)
-except (KeyboardInterrupt, SystemExit):
-    print(setRowCol(10 + quizSize * 2, 17) + '👋  Bye!' + CSI('K') + '\n')
+    try:
+        learnWords(wordList, wordsToLearn)
+    except (KeyboardInterrupt, SystemExit):
+        if 0 < len(wordsToLearn) < repetitions * len(wordList):
+            writeWordList(savename, wordsToLearn)
+            print(setRowCol(9 + quizSize * 2, 17) +
+                  "Saved progress for " + filename)
+
+        print(setRowCol(10 + quizSize * 2, 17) + '👋  Bye!' + CSI('K') + '\n')
+        return False
+
+    return True
+
+
+for filename in (sys.argv[1:] if len(sys.argv) > 1 else ["wordList.txt"]):
+    if not learnFile(filename):
+        break
